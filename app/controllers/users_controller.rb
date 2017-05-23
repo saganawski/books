e03f5f2a84037fbaaa5c4682ede915124f9c030e
Ken Saganski <saganawski@gmail.com>
2017-05-23 18:51:03 -0500

    add new user form, error test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,9 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			session[:user_id] = @user.id 
-			redirect_to root
+			redirect_to root_path
 		else
-			@errors = @users.errors.full_messages
+			@errors = @user.errors.full_messages
 			render 'new'
 		end
 	end
