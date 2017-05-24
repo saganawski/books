f1a23c9009071e9468276536baa16e4600b6da4c
Ken Saganski <saganawski@gmail.com>
2017-05-23 21:51:27 -0500

    add sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :books
-  resources :users
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
   root 'books#index'
 end
