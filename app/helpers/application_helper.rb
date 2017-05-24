437a0e7c211a942bf8c27f1ee8a51b713e516369
Ken Saganski <saganawski@gmail.com>
2017-05-23 21:58:05 -0500

    add logged_in? helper methods and updated to nav page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def logged_in?
+		!!session[:user_id]
+	end
+	
+	def current_user
+		return nil if !logged_in?
+		@current_user ||= User.find_by(id: session[:user_id])
+	end
 end
